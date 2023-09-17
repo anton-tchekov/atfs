81bb8ac37ec967d84e0123e44976d12f5966370f
anton-tchekov <anton@tchekov.net>
2023-09-17 13:19:22 +0000

    Read from device

--- src/atfs_dir.c
+++ src/atfs_dir.c
@@ -1,2 +1,14 @@
 #include "atfs_dir.h"
+#include "dev.h"
+
+void read_chunk(BlockDevice *dev)
+{
+	u8 buffer[dev->BlockSize * NumChunks];
+
+	dev->Read(ChunkAddr, NumChunks, buffer);
+
+	/* Buffer enthält die gelesenen chunks */
+
+	// dev->Write(ChunkAddr, NumChunks, buffer);
+}
 
