e2633832f1c9e635af46471d9ce5a99e4fe8dd5b
anton-tchekov <anton@tchekov.net>
2023-09-17 14:12:20 +0000

    Print chunk function

--- src/dev.c
+++ src/dev.c
@@ -2,9 +2,11 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <assert.h>
+#include <ctype.h>
 
 void dev_print_block(BlockDevice *dev, u32 block)
 {
+	u32 p, i, c;
 	u8 buffer[dev->BlockSize];
 
 	if(dev->Read(block, 1, buffer) != DEVICE_STATUS_OK)
@@ -13,6 +15,26 @@ void dev_print_block(BlockDevice *dev, u32 block)
 		return;
 	}
 
+	printf("Prining block %"PRIu32":\n", block);
+
+	for(p = 0; p < dev->BlockSize; p += 16)
+	{
+		printf("%04x: ", p);
+
+		for(i = 0; i < 16; ++i)
+		{
+			printf("%02x ", buffer[p + i]);
+		}
+
+		printf("[ ");
+		for(i = 0; i < 16; ++i)
+		{
+			c = buffer[p + i];
+			printf("%c", isprint(c) ? c : '.');
+		}
+
+		printf(" ]\n");
+	}
 
 }
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "ramdisk.h"
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
 	BlockDevice *dev = &ramdisk;
+
+	u8 buffer[dev->BlockSize];
+	memset(buffer, 0, sizeof(buffer));
+	strcpy((char *)buffer, "Hello World");
+
+	dev->Write(0, 1, buffer);
 
 	dev_print_block(dev, 0);
 
