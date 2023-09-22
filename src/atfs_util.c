eb310c5a643eb3e4007414006c25579c97037ce9
anton-tchekov <anton@tchekov.net>
2023-09-22 00:04:43 +0000

    Made RO-parameter const and added comments

--- src/atfs_util.c
+++ src/atfs_util.c
@@ -15,7 +15,7 @@ void atfs_write32(u8 *p, u32 v)
 	p[3] = v >> 24;
 }
 
-u32 atfs_read32(u8 *p)
+u32 atfs_read32(const u8 *p)
 {
 	return p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
 }
--- src/atfs_util.h
+++ src/atfs_util.h
@@ -11,7 +11,20 @@
 
 #include "types.h"
 
+/**
+ * @brief Write a unsigned 32-bit integer into a buffer
+ *
+ * @param p Pointer into buffer
+ * @param v 32-bit integer value to write
+ */
 void atfs_write32(u8 *p, u32 v);
-u32 atfs_read32(u8 *p);
+
+/**
+ * @brief Read a unsigned 32-bit integer from a buffer
+ *
+ * @param p Pointer into buffer
+ * @return 32-bit integer that was read
+ */
+u32 atfs_read32(const u8 *p);
 
 #endif /* __ATFS_UTIL_H__ */
