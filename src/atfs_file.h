5c19120bb68b9bb854f443aeed223a3f3af8762f
anton-tchekov <anton@tchekov.net>
2023-09-22 15:46:53 +0000

    Add doxygen comments

--- src/atfs_file.h
+++ src/atfs_file.h
@@ -16,8 +16,26 @@ ATFS_Status atfs_fcreate(BlockDevice *dev, const char *path,
 
 ATFS_Status atfs_fopen(BlockDevice *dev, const char *path, ATFS_File *file);
 
+/**
+ * @brief Read blocks from a file
+ *
+ * @param file Pointer to file struct
+ * @param block First block to read
+ * @param count Number of blocks to read
+ * @param buf Pointer to buffer for read data
+ * @return Status code
+ */
 ATFS_Status atfs_fread(ATFS_File *file, u32 block, u32 count, void *buf);
 
+/**
+ * @brief Write blocks to a file
+ *
+ * @param file Pointer to file struct
+ * @param block First block to write
+ * @param count Number of blocks to write
+ * @param buf Pointer to buffer with data to write
+ * @return Status code
+ */
 ATFS_Status atfs_fwrite(ATFS_File *file, u32 block, u32 count, const void *buf);
 
 #endif /* __ATFS_FILE_H__ */
